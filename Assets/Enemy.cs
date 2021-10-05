d29f8ca56c8fbb868a8ca4733b81d61a1593f7b8
Rift25 <misic.nino@gmail.com>
2021-10-05 02:14:50 -0400

    The AI now slows down once it comes close to the player
    Made it so that the AI starts to slow down once it reaches a certain distance from the player, still got to add acceleration and deceleration.

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -5,15 +5,13 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float speed;
+    public float slowingDistance;
     public float stoppingDistance;
     public float retreatDistance;
 
-    //public int numberOfRays;
-    //public float angle;
-
-    //public float rayRange;
-
     //The player that the enemy has to chase and all the obstacles to avoid crashing into
+    //Need help with this part, dont know how to make it so that it only requires one obstacle for all four spheres
+    //Doesn't work if I just copy/paste multiple obstacles and just add the same tag, tried
     private Transform player;
     private Transform obstacle;
     private Transform obstacle1;
@@ -26,7 +24,7 @@ public class Enemy : MonoBehaviour
         //Here the game finds all the tags and makes it so that the "Enemy" can interact with them
         player = GameObject.FindGameObjectWithTag("Player").transform;
 
-        obstacle = GameObject.FindGameObjectWithTag("Obstacle0").transform;
+        obstacle  = GameObject.FindGameObjectWithTag("Obstacle0").transform;
         obstacle1 = GameObject.FindGameObjectWithTag("Obstacle1").transform;
         obstacle2 = GameObject.FindGameObjectWithTag("Obstacle2").transform;
         obstacle3 = GameObject.FindGameObjectWithTag("Obstacle3").transform;
@@ -35,76 +33,44 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Ignore!!
-        /*var deltaPosition = Vector3.zero;
-        for (int i = 0; i < numberOfRays; i++)
-        {
-            var rotation = this.transform.rotation;
-            var rotationMod = Quaternion.AngleAxis(angle: (i / ((float)numberOfRays - 1)) * angle * 2 - angle, this.transform.up);
-            var direction = rotation * rotationMod * Vector3.forward;
-
-            var ray = new Ray(origin: this.transform.position, direction);
-            RaycastHit hitInfo;
-            if (Physics.Raycast(ray, out hitInfo, rayRange))
-            {
-                deltaPosition -= (1.0f / numberOfRays) * speed * direction;
-            }
-            else
-            {
-                deltaPosition -= (1.0f / numberOfRays) * speed * direction;
-            }
-        }
-        this.transform.position += deltaPosition * Time.deltaTime;*/
-
         //Here the "Enemy" avoids the obstacles if it comes too close
         if(Vector3.Distance(transform.position, obstacle.position) < retreatDistance)
         {
             transform.position = Vector3.MoveTowards(transform.position, obstacle.position, -speed * Time.deltaTime);
-        }else
-        if (Vector3.Distance(transform.position, obstacle1.position) < retreatDistance)
+        }
+        else if (Vector3.Distance(transform.position, obstacle1.position) < retreatDistance)
         {
             transform.position = Vector3.MoveTowards(transform.position, obstacle1.position, -speed * Time.deltaTime);
-        }else
-        if (Vector3.Distance(transform.position, obstacle2.position) < retreatDistance)
+        }
+        else if (Vector3.Distance(transform.position, obstacle2.position) < retreatDistance)
         {
             transform.position = Vector3.MoveTowards(transform.position, obstacle2.position, -speed * Time.deltaTime);
-        }else
-        if (Vector3.Distance(transform.position, obstacle3.position) < retreatDistance)
+        }
+        else if (Vector3.Distance(transform.position, obstacle3.position) < retreatDistance)
         {
             transform.position = Vector3.MoveTowards(transform.position, obstacle3.position, -speed * Time.deltaTime);
         }
 
         //Here the "Enemy" chases the player
-        if (Vector3.Distance(transform.position, player.position) > stoppingDistance)
+        if (Vector3.Distance(transform.position, player.position) > slowingDistance)
         {
             transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
         }
-        //If the "Enemy" comes close it stops chasing
-        else if(Vector3.Distance(transform.position, player.position) < stoppingDistance && Vector3.Distance(transform.position, player.position) > retreatDistance)
+        //If the "Enemy" comes close it starts slowing down
+        else if(Vector3.Distance(transform.position, player.position) < slowingDistance && Vector3.Distance(transform.position, player.position) > stoppingDistance)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.position, 
+                (speed * Vector3.Distance(transform.position, player.position) / slowingDistance) * Time.deltaTime);
+        }
+        //If the "Enemy" comes too close it stops completely
+        else if( Vector3.Distance(transform.position, player.position) < stoppingDistance && Vector3.Distance(transform.position, player.position) > retreatDistance)
         {
             transform.position = this.transform.position;
         }
-        //If the "Enemy" comes too close it starts to retreat
+        //If the "Enemy" enters the retreat distance it starts to retreat
         else if(Vector3.Distance(transform.position, player.position) < retreatDistance)
         {
             transform.position = Vector3.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
         }
-        
-
-       
-
     }
-
-
-    //Ignore!!
-    /*void OnDrawGizmos()
-    {
-         for(int i = 0; i < numberOfRays; i++)
-        {
-            var rotation = this.transform.rotation;
-            var rotationMod = Quaternion.AngleAxis(angle: (i / ((float)numberOfRays - 1)) * angle * 2 - angle, this.transform.up);
-            var direction = rotation * rotationMod * Vector3.forward;
-            Gizmos.DrawRay(from: this.transform.position, direction);
-        }
-    }*/
 }
